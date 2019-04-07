5db7f90283d230a101aabfd3917b254b6f16e0d4
anto <anto@antos-iMac.local>
2019-04-07 22:16:10 +0200

    Enhanced go.sh

--- go.sh
+++ go.sh
@@ -1,4 +1,16 @@
-export HB_PLATFORM="darwin"
-export HB_COMPILER="clang"
-../harbour/bin/darwin/clang/hbmk2 -debug editor -lhbdebug
-./editor
+if [[ $OSTYPE == *"android"* ]]; then
+  export HB_PLATFORM="android"
+  export HB_COMPILER="gcc"
+  ../harbour/bin/android/gcc/hbmk2 hbide.hbp
+fi
+
+if [[ $OSTYPE == *"darwin"* ]]; then
+  export HB_PLATFORM="darwin"
+  export HB_COMPILER="clang"
+  ../harbour/bin/darwin/clang/hbmk2 hbide.hbp
+fi
+
+if [ $? == 0 ]; then
+  ./hbide
+fi
+
